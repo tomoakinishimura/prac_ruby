f0250760ad3e59842469a364a245abbabdf5589e
nishimura tomoaki <nishimura_tomoaki@freeplus.co.jp>
2017-05-17 02:35:14 +0000

    add style

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -8,14 +8,21 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   
   
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select "title", "Home| Ruby on Rails Tutorial Sample App"
   end
+  
+  test "should get contact" do
+    get contact_path
+    assert_response :success
+    assert_select "title", "Contact|Ruby on Rails Tutorial Sample App"
+  end
+  
 end
