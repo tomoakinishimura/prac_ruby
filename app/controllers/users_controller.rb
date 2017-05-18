6f88939b749180c40c02212632b05ff21ef0cb01
nishimura tomoaki <nishimura_tomoaki@freeplus.co.jp>
2017-05-18 06:08:02 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,31 @@
 class UsersController < ApplicationController
+  
   def new
+    @user = User.new
   end
   
   def show 
     @id = params[:id]
     @user = User.find(@id)
   end
+  
+  def create 
+    @user = User.new(user_params)
+    
+    if @user.save
+      #保存の成功をここで扱う
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                  :password_confirmation)
+    end
+  
   
 end
